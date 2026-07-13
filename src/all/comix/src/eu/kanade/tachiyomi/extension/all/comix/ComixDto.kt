20bd75fd9a5ef0bd4a74969f384c27a166112b51
marbou92 <mr.marb1930@gmail.com>
2026-07-13 23:28:18 +0100

    Add files via upload

--- src/all/comix/src/eu/kanade/tachiyomi/extension/all/comix/ComixDto.kt
+++ src/all/comix/src/eu/kanade/tachiyomi/extension/all/comix/ComixDto.kt
@@ -1,6 +1,5 @@
 package eu.kanade.tachiyomi.extension.all.comix
 
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.JsonElement
 
